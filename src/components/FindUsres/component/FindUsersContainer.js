582ccbd421abe5d9fad6600b9e4c5c18faea4ada
Stan <civilizator@hotmail.com>
2019-10-20 01:47:07 +0800

    Added Preloader for Find Users

--- src/components/FindUsres/component/FindUsersContainer.js
+++ src/components/FindUsres/component/FindUsersContainer.js
@@ -1,37 +1,59 @@
-
-import {connect} from "react-redux"
-import { setUsersCreator, userFollowCreator, activePageCreator, totalUsersCountCreator } from "../../../redux/find-users-reducer"
+import { connect } from "react-redux"
+import {
+    setUsersCreator,
+    userFollowCreator,
+    activePageCreator,
+    totalUsersCountCreator,
+    eventPreloaderCreator
+} from "../../../redux/find-users-reducer"
 import React from "react";
 import * as axios from "axios";
 import FindUsers from "./FindUsers";
+import classes from "./FindUsersLoader.module.scss"
+
+const UsersLoaderComponent = () => {
+    return (
+        <div className={ classes.containerLoader }>
+            <div className={ classes.ldsHourglass }>
+            </div>
+        </div>
+    )
+}
 
 class FindUsersContainer extends React.Component {
 
     pageChange = (page) => {
+        this.props.eventPreloader( !0 )
         this.props.setActivePage( page )
         axios.get( `https://social-network.samuraijs.com/api/1.0/users?page=${ page }&count=${ this.props.pageSize }` )
             .then( res => {
                 this.props.setUsers( res.data.items )
                 this.props.setTotalUsersCount( res.data.totalCount )
+                this.props.eventPreloader( !1 )
             } )
     }
 
     componentDidMount() {
         // if (this.props.users.length === 0) {
-            this.pageChange( this.props.currentPage )
+        this.pageChange( this.props.currentPage )
         // }
     }
 
     render() {
         return (
-            <FindUsers
-                users={ this.props.users }
-                setFollowToUser={ this.props.setFollowToUser }
-                totalUsersCount={ this.props.totalUsersCount }
-                pageSize={ this.props.pageSize }
-                currentPage={ this.props.currentPage }
-                pageChange={ this.pageChange }
-            />
+            <>
+
+                { this.props.isFetching ? <UsersLoaderComponent/> : null }
+
+                <FindUsers
+                    users={ this.props.users }
+                    setFollowToUser={ this.props.setFollowToUser }
+                    totalUsersCount={ this.props.totalUsersCount }
+                    pageSize={ this.props.pageSize }
+                    currentPage={ this.props.currentPage }
+                    pageChange={ this.pageChange }
+                />
+            </>
         )
     }
 }
@@ -49,19 +71,22 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
     return {
         setUsers: (users) => {
-            dispatch(setUsersCreator(users))
+            dispatch( setUsersCreator( users ) )
         },
         setFollowToUser: (userId) => {
-            dispatch(userFollowCreator(userId))
+            dispatch( userFollowCreator( userId ) )
         },
         setActivePage: (numberPage) => {
-            dispatch(activePageCreator(numberPage))
+            dispatch( activePageCreator( numberPage ) )
         },
         setTotalUsersCount: (totalUsersCount) => {
-            dispatch(totalUsersCountCreator(totalUsersCount))
+            dispatch( totalUsersCountCreator( totalUsersCount ) )
+        },
+        eventPreloader: (event) => {
+            dispatch( eventPreloaderCreator( event ) )
         }
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(FindUsersContainer)
+export default connect( mapStateToProps, mapDispatchToProps )( FindUsersContainer )
 
--- src/redux/find-users-reducer.js
+++ src/redux/find-users-reducer.js
@@ -2,21 +2,22 @@ const SET_USERS = 'SET-USERS'
 const FOLLOW_TO_USER = 'FOLLOW-TO-USER'
 const ACTIVE_PAGE = 'ACTIVE-PAGE'
 const TOTAL_USERS_COUNT = 'TOTAL-USERS-COUNT'
+const EVENT_PRELOADER = 'EVENT-PRELOADER'
 
 const initialState = {
     users: [
-        { id: 1, followed: true, name: "Maria K.", userCity: "City", userCountry: "Country", userStatus: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, veritatis!" },
-        { id: 2, followed: false, name: "John", userCity: "City", userCountry: "Country", userStatus: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, veritatis!"  },
-        { id: 3, followed: false, name: "Sarah", userCity: "City", userCountry: "Country", userStatus: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, veritatis!"  },
-        { id: 4, followed: true, name: "Jax", userCity: "City", userCountry: "Country", userStatus: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, veritatis!"  },
-        { id: 5, followed: false, name: "Helen", userCity: "City", userCountry: "Country", userStatus: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, veritatis!"  },
-        { id: 6, followed: false, name: "Brain", userCity: "City", userCountry: "Country", userStatus: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, veritatis!"  },
-        { id: 7, followed: true, name: "Katrine", userCity: "City", userCountry: "Country", userStatus: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, veritatis!"  }
+        // { id: 1, followed: true, name: "Maria K.", userCity: "City", userCountry: "Country", userStatus: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, veritatis!" },
+        // { id: 2, followed: false, name: "John", userCity: "City", userCountry: "Country", userStatus: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, veritatis!"  },
+        // { id: 3, followed: false, name: "Sarah", userCity: "City", userCountry: "Country", userStatus: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, veritatis!"  },
+        // { id: 4, followed: true, name: "Jax", userCity: "City", userCountry: "Country", userStatus: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, veritatis!"  },
+        // { id: 5, followed: false, name: "Helen", userCity: "City", userCountry: "Country", userStatus: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, veritatis!"  },
+        // { id: 6, followed: false, name: "Brain", userCity: "City", userCountry: "Country", userStatus: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, veritatis!"  },
+        // { id: 7, followed: true, name: "Katrine", userCity: "City", userCountry: "Country", userStatus: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, veritatis!"  }
     ]
-    , totalUsersCount: 14
+    , totalUsersCount: 15
     , pageSize: 10
     , currentPage: 1
-    , isFetching: false
+    , isFetching: !1
 }
 
 const findUsersReducer = (state = initialState, action) => {
@@ -66,6 +67,13 @@ const findUsersReducer = (state = initialState, action) => {
             }
         }
 
+        case EVENT_PRELOADER: {
+            return {
+                ...state
+                , isFetching: action.event
+            }
+        }
+
         default:
             return state
     }
@@ -83,6 +91,9 @@ export const
     }
     , totalUsersCountCreator = (totalUsersCount) => {
         return { type: TOTAL_USERS_COUNT, totalUsersCount: totalUsersCount }
+    }
+    , eventPreloaderCreator = (event) => {
+        return { type: EVENT_PRELOADER, event: event }
     }
 
 export default findUsersReducer
